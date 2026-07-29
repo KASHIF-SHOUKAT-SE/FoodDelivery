75f191cdeabfb52f9e743557b4dad4e4adf5bd77
Muhammad Asnaif Asim <asnaifkhanasim@gmail.com>
2026-07-29 11:55:56 +0500

    Resolve FriesSection merge conflict

--- src/component/Restaurents/Categories/FriesSection.jsx
+++ src/component/Restaurents/Categories/FriesSection.jsx
@@ -7,9 +7,9 @@ const FriesSection = () => {
 
       <div className="max-w-[1528px] mx-auto px-5">
 
-            <h2 className="text-[44px] font-bold text-[#FC8A06] mb-10">
-            Fries
-            </h2>
+        <h2 className="text-[44px] font-bold text-[#FC8A06] mb-10">
+          Fries
+        </h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
           {friesData.map((fries) => (
